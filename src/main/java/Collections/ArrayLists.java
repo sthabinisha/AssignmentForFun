bb218b969fbe7d78926da82916e33755dbab90bb
Binisha Shrestha <shrestha.binishaa@gmail.com>
2021-03-27 18:30:24 -0400

    basic string done

--- src/main/java/Collections/ArrayLists.java
+++ src/main/java/Collections/ArrayLists.java
@@ -5,17 +5,31 @@ import java.util.ArrayList;
 public class ArrayLists {
 
     public ArrayList<Integer> addShit(ArrayList<Integer> list1, ArrayList<Integer> list2) {
-        ArrayList<Integer> result = new ArrayList<Integer>();
+            list1.addAll(list2);
 
-        return result;
+
+
+        return list1;
     }
 
     public Integer addShitTogether(ArrayList<Integer> list1, ArrayList<Integer> list2) {
-        return null;
+        Integer res = 0;
+       for(int i = 0; i< list1.size(); i++){
+           res += list1.get(i);
+
+       }
+        for(int i = 0; i< list2.size(); i++){
+            res += list2.get(i);
+
+        }
+        return res;
     }
 
     public ArrayList<Integer> removeAll(ArrayList<Integer> original, Integer toRemove) {
-        return null;
+       if(original.contains(toRemove)){
+           original.remove(original.indexOf(toRemove));
+       }
+       return original;
     }
 
     public boolean happyList(ArrayList<String> original) {
--- src/main/java/Collections/MapPractice.java
+++ src/main/java/Collections/MapPractice.java
@@ -1,22 +1,67 @@
 package Collections;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+
 
 public class MapPractice {
 
     public Object findValueOf(Map map, Object key) {
-        return null;
+       return map.get(key);
     }
 
     public Object[] findKeysOf(Map map, Object value) {
-        return null;
+        ArrayList<String> res = new ArrayList<String>();
+        Set<String> keys = map.keySet();
+        int i = 0;
+      for(String s: keys){
+          if(map.get(s).equals(value)){
+              res.add(s);
+          }
+
+      }
+        return res.toArray();
     }
 
     public Map<Integer, Integer> fibonacciTree(int size) {
-        return null;
+        Map<Integer, Integer> results = new HashMap<Integer, Integer>();
+
+        for(int i= 1; i<=size; i++){
+           results.put(i, fibo(i)) ;
+        }
+        return results;
+    }
+
+    private int fibo(int i) {
+        if(i <=2){
+            return 1;
+        }else{
+            int v = fibo(i-1)+ fibo(i-2);
+            return v;
+        }
     }
 
     public Map<Integer, Integer> crazySpiral(Integer first, Integer second, Integer size) {
-        return null;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        if (size >= 3) {
+            map.put(1, first);
+            map.put(2, second);
+            for (int i = 3; i <= size; i++) {
+
+                int val = first + second;
+
+                map.put(i, val);
+                first = second;
+                second = val;
+            }
+
+        }else if(size==1){
+            map.put(1, first);
+        }
+        return map;
     }
+
+
 }
